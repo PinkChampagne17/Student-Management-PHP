a7072f8f602ee346f6f32eae387912eea0824224
RikeyZero <83920026+RikeyZero@users.noreply.github.com>
2023-04-04 17:04:29 +0800

    Update connect_db.php

--- untils/connect_db.php
+++ untils/connect_db.php
@@ -1,5 +1,6 @@
 <?php
     // 创建连接
+    //输入你自己的的数据库连接:"连接地址"，“登入账号”，“登入密码”，“使用数据库”,"端口"
     $conn = new mysqli("localhost", "root", "12304has", "student_management", 3307);
 
     $conn->set_charset("utf8");
